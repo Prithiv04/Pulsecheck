a109defa1abcd621b7225db209e6090408f4156c
Prithiv R <Shakilaprithiv9677273879@gmail.com>
2025-12-08 10:03:18 +0530

    Delete pulsecheck directory

--- pulsecheck/backend/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('./controllers');
-
-router.post('/saveReading', controller.saveReading);
-router.get('/history', controller.getHistory);
-
-module.exports = router;
